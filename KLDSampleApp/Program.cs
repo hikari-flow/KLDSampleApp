e39bd1609711fdb426cec47f0f4385daed3325c3
Austin Tran <autramusic@gmail.com>
2022-06-08 10:09:45 -0500

    Added user input

--- KLDSampleApp/Program.cs
+++ KLDSampleApp/Program.cs
@@ -6,7 +6,13 @@ namespace KLDSampleApp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Input Path: ");
+            string inputPath = Console.ReadLine();
+
+            Console.Write("Output Path: ");
+            string outputPath = Console.ReadLine();
+
+            Console.WriteLine($"{inputPath}\n{outputPath}");
         }
     }
 }
